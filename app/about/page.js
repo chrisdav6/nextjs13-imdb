e07968587e95143fa9fdf5b772218e94c0fb2f0a
chris.davis5440@gmail.com <chris.davis5440@gmail.com>
2023-03-21 15:08:02 -0400

    Created the About page

---
+++ app/about/page.js
@@ -0,0 +1,17 @@
+const AboutPage = () => {
+  return (
+    <div className='max-w-6xl mx-auto space-y-4'>
+      <h1 className='text-2xl font-medium text-amber-600'>About</h1>
+      <p>
+        Launched online in 1990 and a subsidiary of Amazon.com since 1998, IMDb
+        is the world's most popular and authoritative source for movie, TV and
+        celebrity content, designed to help fans explore the world of movies and
+        shows and decide what to watch. Our searchable database includes
+        millions of movies, TV and entertainment programs and cast and crew
+        members.
+      </p>
+    </div>
+  );
+};
+
+export default AboutPage;
