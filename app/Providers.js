56330a57b0137c995c3d2b40bd2f6233d6c2e8cd
chris.davis5440@gmail.com <chris.davis5440@gmail.com>
2023-03-21 14:41:53 -0400

    Added light and dark mode selector

--- app/Providers.js
+++ app/Providers.js
@@ -2,7 +2,11 @@
 import { ThemeProvider } from 'next-themes';
 
 const Providers = ({ children }) => {
-  return <ThemeProvider>{children}</ThemeProvider>;
+  return (
+    <ThemeProvider enableSystem={true} attribute='class'>
+      <div className='modeSwitch'>{children}</div>
+    </ThemeProvider>
+  );
 };
 
 export default Providers;
--- app/components/DarkModeSwitch.js
+++ app/components/DarkModeSwitch.js
@@ -1,5 +1,33 @@
+'use client';
+import { useEffect, useState } from 'react';
+import { useTheme } from 'next-themes';
+import { BsFillMoonStarsFill, BsFillSunFill } from 'react-icons/bs';
+
 const DarkModeSwitch = () => {
-  return <div>DarkModeSwitch</div>;
+  const { systemTheme, theme, setTheme } = useTheme();
+  const currentTheme = theme === 'system' ? systemTheme : theme;
+
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  return (
+    <div>
+      {mounted && currentTheme === 'dark' ? (
+        <BsFillSunFill
+          className='text-xl cursor-pointer hover:text-amber-500'
+          onClick={() => setTheme('light')}
+        />
+      ) : (
+        <BsFillMoonStarsFill
+          className='text-xl cursor-pointer hover:text-amber-500'
+          onClick={() => setTheme('dark')}
+        />
+      )}
+    </div>
+  );
 };
 
 export default DarkModeSwitch;
